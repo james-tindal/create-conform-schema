846876bee75a3d49972ba3ac37460dfaee234301
james-tindal <10291002+james-tindal@users.noreply.github.com>
2024-08-13 14:24:39 +0100

    refactor: remove makeRefinementFunction

--- app/createSchema.ts
+++ app/createSchema.ts
@@ -17,12 +17,14 @@ type Refinements<Name extends string> = {
 
 type ServerValidationNames = 'isUsernameUnique'
 
+// function that:
+// maps predicates to refinements
 
 export function createFormSchema(
-  create: (server: Refinements<ServerValidationNames>) => ZodSchema
+  define: (server: Refinements<ServerValidationNames>) => ZodSchema
 ) {
   const schemaCreator = (server?: Predicates<ServerValidationNames>) =>
-    create({
+    define({
       isUsernameUnique: makeRefinement(server?.isUsernameUnique)
     })
 
@@ -32,8 +34,8 @@ export function createFormSchema(
   }
 }
 
-const addIssueOnFail = async (asyncTruthy: AsyncTruthy, message: string, ctx: RefinementCtx) => {
-	const truthy = await asyncTruthy
+const addIssueOnFail = (message: string, predicate: Predicate) => async (input: any, ctx: RefinementCtx) => {
+	const truthy = await predicate(input)
 	if (! truthy)
 		ctx.addIssue({
 			code: 'custom',
@@ -41,19 +43,21 @@ const addIssueOnFail = async (asyncTruthy: AsyncTruthy, message: string, ctx: Re
 		})
 }
 
-const validateOnServer = (ctx: RefinementCtx) => ctx.addIssue({
+const validateOnServer = (input: any, ctx: RefinementCtx) => ctx.addIssue({
 	code: 'custom',
 	message: conformZodMessage.VALIDATION_UNDEFINED,
 	fatal: true,
 })
 
-const makeRefinementFunction = (message: string, predicate?: Predicate) => (input: any, ctx: RefinementCtx) => {
-	if (! predicate)
-		return validateOnServer(ctx)
-
-	const asyncTruthy = predicate(input)
-	return addIssueOnFail(asyncTruthy, message, ctx)
-}
-
 export const makeRefinement = (predicate?: Predicate) => (message: string) =>
-  z.string().superRefine(makeRefinementFunction(message, predicate))
+  z.unknown().superRefine(
+    predicate ? addIssueOnFail(message, predicate) : validateOnServer)
+
+// Now how to add skips to this model?
+// Every server validation checks the intent and skips if irrelevant
+
+// I could also add helpers to the "define" function
+// arg is "context"
+// ctx.intent
+// ctx.skip -> function to skip this validation
+// ctx.skip -> object of supplied skip functions.  Not at all sure this has any value
